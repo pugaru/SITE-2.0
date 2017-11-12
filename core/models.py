7122a9d55e34c39abc57d502746d57396a0834ce
AllanTuratti <30848823+AllanTuratti@users.noreply.github.com>
2017-11-12 19:28:11 -0200

    Update models.py
    terminando a parte do cadastro

--- core/models.py
+++ core/models.py
@@ -1,33 +1,62 @@
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+# Create your models here.
+
+class UsuarioManager(BaseUserManager):
+    use_in_migrations = True
+    def _create_user(self, ra, password, **extra_fields):
+        if not ra:
+            raise ValueError('RA precisa ser preenchido')
+        user = self.model(ra=ra, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_user(self, ra, password=None, **extra_fields):
+        return self._create_user(ra, password, **extra_fields)
+    def create_superuser(self, ra, password, **extra_fields):
+        return self._create_user(ra, password, **extra_fields)
 
 
-class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+class Usuario(AbstractBaseUser):
+    nome = models.CharField(max_length= 100)
+    ra = models.IntegerField(unique=True)
+    password = models.CharField(max_length= 150)
+    email = models.CharField(max_length=100)
+    perfil = models.CharField(max_length= 1, default='C')
+    ativo = models.BooleanField(default=True)
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    USERNAME_FIELD = 'ra'
+    REQUIRED_FIELDS = ['nome']
 
-    def __str__(self):
-        return self.title
+    objects = UsuarioManager()
+    
+    @property
+    def is_staff(self):
+        return self.perfil == 'C'
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    def has_module_perms(self, app_label):
+        return True
+
+    def get_short_name(self):
+        return True
+    def get_full_nama(self):
+        return self.nome
+
 
 class Curso(models.Model):
-        
-        sigla = models.CharField(max_length=10)
-        nome = models.CharField(max_length=200)
-        tipo = models.CharField(max_length=50,blank=True)
-        carga_horaria = models.IntegerField(default=1000)
-        ativo = models.BooleanField(default=True)
+    siglas = models.CharField(max_length= 5)
+    nome = models.CharField(max_length= 100)
+    tipo = models.CharField(max_length= 50, blank=True)
+    carga_horaria = models.IntegerField(default = 1000)
+    ativo = models.BooleanField(default=True)
+    descricao = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.nome
 
-        descricao = models.TextField(blank=True)
-        
-        def __str__(self):
-                return self.nome
-# Create your models here.
+class Aluno(Usuario):
+    curso = models.ForeignKey(
+        Curso
+    )
