8e64d8940bdf78d21400d4dccb36364e59153889
pugaru <30847672+pugaru@users.noreply.github.com>
2017-11-13 19:39:48 -0200

    Update settings.py

--- lmsimpacta/settings.py
+++ lmsimpacta/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'lmsimpacta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'localdb',
+        'USER': 'azure',
+        'PASSWORD': '6#vWHD_$',
+        'HOST': '127.0.0.1',
+        'PORT': '53738'
     }
 }
-
 
 
 # Password validation
