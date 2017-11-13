04ce08f3fc0f9749989ec630aa4e0691f7a583fd
pugaru <30847672+pugaru@users.noreply.github.com>
2017-11-13 14:28:42 -0200

    Update settings.py

--- lmsimpacta/settings.py
+++ lmsimpacta/settings.py
@@ -76,15 +76,10 @@ WSGI_APPLICATION = 'lmsimpacta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'localdb',
-        'USER': 'usuario',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
-        'PORT': '53738'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 
 
