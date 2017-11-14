166fd0660004c8c7efd25671b8f3930f13140c98
pugaru <30847672+pugaru@users.noreply.github.com>
2017-11-14 21:35:44 -0200

    Update settings.py

--- lmsimpacta/settings.py
+++ lmsimpacta/settings.py
@@ -77,7 +77,6 @@ WSGI_APPLICATION = 'lmsimpacta.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
